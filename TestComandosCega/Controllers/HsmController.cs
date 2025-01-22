6efeabe7a503427494ae3e21f7a741273c2542a7
Usuario3-Cega <Usuario3-Cega@DESKTOP-KQSLUUP>
2025-01-22 09:43:25 -0600

    Agregar archivos de proyecto.

---
+++ TestComandosCega/Controllers/HsmController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestComandosCega.Models;
+
+namespace TestComandosCega.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HsmController : Controller
+    {
+        [HttpGet("info")]
+        public IActionResult GetHsmInfo()
+        {
+            string command = "cxitool"; // Cambia esto por el comando adecuado
+            string arguments = "Dev=3001@127.0.0.1 LogonPass=usuario1,123456 Name=Key2 Group=usuario1 DeleteKey"; // Cambia esto por los argumentos que necesites
+
+            Console.WriteLine("Ejecutando comando");
+            HsmCommandExecutor hsmCommandExecutor = new HsmCommandExecutor();
+            string result = HsmCommandExecutor.ExecuteCommandAsync(command, arguments).Result;
+            Console.WriteLine(result);
+
+            if (result.StartsWith("Error:"))
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ TestComandosCega/Models/HsmCommandExecutor.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+namespace TestComandosCega.Models
+{
+    public class HsmCommandExecutor
+    {
+        public static async Task<string> ExecuteCommandAsync(string command, string arguments, int timeoutMilliseconds = 5000)
+        {
+            try
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                {
+                    FileName = command,
+                    Arguments = arguments,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = processStartInfo;
+                    process.Start();
+
+                    string outputTask = await process.StandardOutput.ReadToEndAsync();
+                    string errorTask = await process.StandardError.ReadToEndAsync();
+
+                    await process.WaitForExitAsync();
+
+                    if (process.ExitCode != 0)
+                    {
+                        throw new Exception($"Error ejecutando comando: {errorTask}");
+                    }
+
+                    return outputTask.ToString();
+                }
+            }
+			catch (Exception ex)
+			{
+                return $"Error: {ex.Message}";
+            }
+        }   
+    }
+}
